7553735a3a4f44016bc532efe9d4e93790e62275
rockstargett@gmail.com <rockstargett@gmail.com>
2023-08-22 18:15:51 +0300

    Initial commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ProductList from './components/ProductList';
 import FixedColumn from './components/FixedColumn';
 import ProductDetail from './components/ProductDetail';
 
+interface Product {
+  // Define your product properties here
+  id: number;
+  name: string;
+  // ... other properties
+}
+
 const App: React.FC = () => {
-  // Fetch likedProducts data here if needed
-  const likedProducts = /* Fetch likedProducts from API or state */;
+  // Initialize state for likedProducts
+  const [likedProducts, setLikedProducts] = useState<Product[]>([]);
+
+  // Fetch likedProducts data from API or state
+  useEffect(() => {
+    // Replace this with actual fetch logic or state update
+    // For example, fetching likedProducts from an API
+    const fetchLikedProducts = async () => {
+      try {
+        // Fetch data from API
+        const response = await fetch('your-api-url-here');
+        const data = await response.json();
+
+        // Update likedProducts state with fetched data
+        setLikedProducts(data);
+      } catch (error) {
+        console.error('Error fetching likedProducts:', error);
+      }
+    };
+
+    fetchLikedProducts(); // Call the fetch function
+  }, []); // Empty dependency array means this effect runs once on mount
 
   return (
     <div>
       <ProductList />
-      <FixedColumn likedProducts={likedProducts} />
-      <ProductDetail />
+      
     </div>
   );
 };
